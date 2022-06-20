178cbbdd8c335fc51075bd8c8bb1c78c724c53e9
kukoskid <101594042+kukoskid@users.noreply.github.com>
2022-06-20 17:23:03 +0200

    Add files via upload
    1 zadaca od zadaci za cas

---
+++ vezbacas.js
@@ -0,0 +1,16 @@
+const moduleFaker = require('@faker-js/faker');
+
+let people=[];
+for(i=0;i<+10;i++){
+    let first_name=moduleFaker.faker.name.firstName();
+    let address=moduleFaker.faker.address.city();
+    let phone=moduleFaker.faker.phone.phoneNumber();
+    let vehicle=moduleFaker.faker.vehicle.vehicle();
+    people.push({
+        first_name:first_name,
+        address:address,
+        phone:phone,
+        vehicle:vehicle
+    })
+}
+console.log(people);
